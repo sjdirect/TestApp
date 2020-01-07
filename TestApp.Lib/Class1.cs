81e67f82a799946046871cbb6459f4333809af11
sjdirect <sjdirect@gmail.com>
2020-01-07 04:01:30 -0800

    first commit

---
+++ TestApp.Lib/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace TestApp.Lib
+{
+    public class Class1
+    {
+        public int GetARandomInt()
+        {
+            return new Random().Next();
+        }
+
+
+        public string GetARandomString(int length)
+        {
+            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            return new string(Enumerable.Repeat(chars, length)
+                .Select(s => s[new Random().Next(s.Length)]).ToArray());
+        }
+    }
+}
---
+++ TestApp/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using TestApp.Lib;
+
+namespace TestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var class1 = new Class1();
+            Console.WriteLine("Random Int 1: " + class1.GetARandomInt());
+            Console.WriteLine("Random Int 2: " + class1.GetARandomInt());
+            Console.WriteLine("Random String 1: " + class1.GetARandomString(4));
+            Console.WriteLine("Random String 2: " + class1.GetARandomString(6));
+        }
+    }
+}
